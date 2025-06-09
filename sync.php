9495e3d9488c8f3fabe6ee7567c618bba312a158
jeronimoAbdala <jeroabdalala@gmail.com>
2025-06-09 11:28:45 -0300

    syncPhp

--- sync.php
+++ sync.php
@@ -1,6 +1,6 @@
 <?php
 // Conexión
-$conexion = new mysqli("localhost", "usuario", "contraseña", "nombre_bd");
+$conexion = new mysqli("limegreen-lion-509134.hostingersite.com", "u975065804_jeroabdalala", "TecladoRaton1-", "u975065804_tecstoreDB");
 
 if ($conexion->connect_error) {
     die("Conexión fallida: " . $conexion->connect_error);
